09b96873f166a98731155f57e774e2a8b2b1e6d4
Marc Espin <marc@mespin.me>
2025-05-20 21:46:10 +0200

    docs

--- src/query.rs
+++ src/query.rs
@@ -382,14 +382,27 @@ pub fn use_query<Q: QueryCapability>(query: Query<Q>) -> UseQuery<Q> {
     UseQuery { query }
 }
 
-#[derive(Clone, PartialEq, Eq)]
-pub struct Captured<T: Clone + PartialEq + Eq>(pub T);
+/// Capture values to use later inside Queries, but with a catch, if the capture value changes the query wont recapture it because
+/// the [PartialEq] implementation always returns false.
+///
+/// So in other words `Capture(1) == Capture(1)` will be `false`.
+///
+/// **This is intended to use for value types that are not mean to be diffed and that are expected to maintain their value across time.
+/// Like "Clients" of external resources.**
+#[derive(Clone)]
+pub struct Captured<T: Clone>(pub T);
 
-impl<T: Clone + PartialEq + Eq> Hash for Captured<T> {
+impl<T: Clone> Hash for Captured<T> {
     fn hash<H: std::hash::Hasher>(&self, _state: &mut H) {}
 }
 
-impl<T: Clone + PartialEq + Eq> Deref for Captured<T> {
+impl<T: Clone> PartialEq for Captured<T> {
+    fn eq(&self, _other: &Self) -> bool {
+        false
+    }
+}
+
+impl<T: Clone> Deref for Captured<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
