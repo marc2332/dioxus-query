00b0638c39610b81c21814ef8d59c9582de62536
Sabin Regmi <get2sabin@gmail.com>
2025-06-14 12:07:26 -0400

    macro cleanup

--- dioxus-query-macro/src/lib.rs
+++ dioxus-query-macro/src/lib.rs
@@ -19,6 +19,59 @@ use syn::{parse_macro_input, Data, DeriveInput, Fields, Lit, Meta, MetaNameValue
 /// ```
 #[proc_macro_derive(Query, attributes(query))]
 pub fn derive_query(input: TokenStream) -> TokenStream {
+    derive_capability(input, CapabilityType::Query)
+}
+
+#[proc_macro_derive(Mutation, attributes(mutation))]
+pub fn derive_mutation(input: TokenStream) -> TokenStream {
+    derive_capability(input, CapabilityType::Mutation)
+}
+
+#[derive(Clone, Copy)]
+enum CapabilityType {
+    Query,
+    Mutation,
+}
+
+impl CapabilityType {
+    fn attribute_name(&self) -> &'static str {
+        match self {
+            CapabilityType::Query => "query",
+            CapabilityType::Mutation => "mutation",
+        }
+    }
+
+    fn default_ok_type(&self) -> proc_macro2::TokenStream {
+        match self {
+            CapabilityType::Query => quote! { String },
+            CapabilityType::Mutation => quote! { () },
+        }
+    }
+
+    fn trait_path(&self) -> proc_macro2::TokenStream {
+        match self {
+            CapabilityType::Query => quote! { ::dioxus_query::query::QueryCapability },
+            CapabilityType::Mutation => quote! { ::dioxus_query::mutation::MutationCapability },
+        }
+    }
+
+    fn additional_methods(&self) -> proc_macro2::TokenStream {
+        match self {
+            CapabilityType::Query => quote! {},
+            CapabilityType::Mutation => quote! {
+                // Add forwarding for on_settled
+                async fn on_settled(&self, keys: &Self::Keys, result: &Result<Self::Ok, Self::Err>) {
+                    // This assumes the user has an inherent method `on_settled` with the same signature.
+                    // If not, this will cause a compile error, which is a way to enforce the contract.
+                    // A more advanced macro could check for the method's existence and provide a true default if not found.
+                    self.on_settled(keys, result).await
+                }
+            },
+        }
+    }
+}
+
+fn derive_capability(input: TokenStream, capability_type: CapabilityType) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
     let (name, fields) = match extract_name_and_fields(&derive_input) {
         Ok(val) => val,
@@ -29,15 +82,22 @@ pub fn derive_query(input: TokenStream) -> TokenStream {
         key_type,
         ok_type,
         err_type,
-    } = match extract_attribute_values(&derive_input.attrs, "query", quote! {String}) {
+    } = match extract_attribute_values(
+        &derive_input.attrs,
+        capability_type.attribute_name(),
+        capability_type.default_ok_type(),
+    ) {
         Ok(val) => val,
         Err(err) => return err.to_compile_error().into(),
     };
 
     let (_, clone_impl) = generate_clone_implementation(&name, fields);
+    let trait_path = capability_type.trait_path();
+    let additional_methods = capability_type.additional_methods();
+    let common_trait_impls = generate_common_trait_impls(&name);
 
     let expanded = quote! {
-        impl ::dioxus_query::query::QueryCapability for #name {
+        impl #trait_path for #name {
             type Ok = #ok_type;
             type Err = #err_type;
             type Keys = #key_type;
@@ -45,10 +105,20 @@ pub fn derive_query(input: TokenStream) -> TokenStream {
             async fn run(&self, key: &Self::Keys) -> Result<Self::Ok, Self::Err> {
                 self.run(key).await
             }
+
+            #additional_methods
         }
 
         #clone_impl
+        #common_trait_impls
+    };
 
+    TokenStream::from(expanded)
+}
+
+/// Generate common trait implementations (PartialEq, Eq, Hash) for both Query and Mutation
+fn generate_common_trait_impls(name: &syn::Ident) -> proc_macro2::TokenStream {
+    quote! {
         impl ::std::cmp::PartialEq for #name {
             fn eq(&self, other: &Self) -> bool {
                 true // For simplicity, consider all instances equal
@@ -62,9 +132,7 @@ pub fn derive_query(input: TokenStream) -> TokenStream {
                 stringify!(#name).hash(state);
             }
         }
-    };
-
-    TokenStream::from(expanded)
+    }
 }
 
 fn extract_name_and_fields(
@@ -141,63 +209,7 @@ fn generate_clone_implementation(
     }
 }
 
-#[proc_macro_derive(Mutation, attributes(mutation))]
-pub fn derive_mutation(input: TokenStream) -> TokenStream {
-    let derive_input = parse_macro_input!(input as DeriveInput);
-    let (name, fields) = match extract_name_and_fields(&derive_input) {
-        Ok(val) => val,
-        Err(err) => return err.to_compile_error().into(),
-    };
 
-    let DeriveAttributeValues {
-        key_type,
-        ok_type,
-        err_type,
-    } = match extract_attribute_values(&derive_input.attrs, "mutation", quote! {()}) {
-        Ok(val) => val,
-        Err(err) => return err.to_compile_error().into(),
-    };
-
-    let (_, clone_impl) = generate_clone_implementation(&name, fields);
-
-    let expanded = quote! {
-        impl ::dioxus_query::mutation::MutationCapability for #name {
-            type Ok = #ok_type;
-            type Err = #err_type;
-            type Keys = #key_type;
-
-            async fn run(&self, key: &Self::Keys) -> Result<Self::Ok, Self::Err> {
-                self.run(key).await
-            }
-
-            // Add forwarding for on_settled
-            async fn on_settled(&self, keys: &Self::Keys, result: &Result<Self::Ok, Self::Err>) {
-                // This assumes the user has an inherent method `on_settled` with the same signature.
-                // If not, this will cause a compile error, which is a way to enforce the contract.
-                // A more advanced macro could check for the method's existence and provide a true default if not found.
-                self.on_settled(keys, result).await
-            }
-        }
-
-        #clone_impl
-
-        impl ::std::cmp::PartialEq for #name {
-            fn eq(&self, other: &Self) -> bool {
-                true // For simplicity, consider all instances equal
-            }
-        }
-
-        impl ::std::cmp::Eq for #name {}
-
-        impl ::std::hash::Hash for #name {
-            fn hash<H: ::std::hash::Hasher>(&self, state: &mut H) {
-                stringify!(#name).hash(state);
-            }
-        }
-    };
-
-    TokenStream::from(expanded)
-}
 
 struct DeriveAttributeValues {
     key_type: proc_macro2::TokenStream,
@@ -225,63 +237,9 @@ fn extract_attribute_values(
                         if let Meta::NameValue(MetaNameValue { path, value, .. }) = meta_item {
                             let ident_name = path.get_ident().map(|i| i.to_string());
                             match ident_name.as_deref() {
-                                Some("key") => {
-                                    if let syn::Expr::Path(expr_path) = value {
-                                        key_type = quote! { #expr_path };
-                                    } else if let syn::Expr::Lit(lit) = value {
-                                        if let Lit::Str(lit_str) = lit.lit {
-                                            let type_ident: syn::Type =
-                                                syn::parse_str(&lit_str.value()).map_err(|e| {
-                                                    syn::Error::new_spanned(
-                                                        lit_str,
-                                                        format!(
-                                                            "Failed to parse key type string: {}",
-                                                            e
-                                                        ),
-                                                    )
-                                                })?;
-                                            key_type = quote! { #type_ident };
-                                        }
-                                    }
-                                }
-                                Some("ok") => {
-                                    if let syn::Expr::Path(expr_path) = value {
-                                        ok_type = quote! { #expr_path };
-                                    } else if let syn::Expr::Lit(lit) = value {
-                                        if let Lit::Str(lit_str) = lit.lit {
-                                            let type_ident: syn::Type =
-                                                syn::parse_str(&lit_str.value()).map_err(|e| {
-                                                    syn::Error::new_spanned(
-                                                        lit_str,
-                                                        format!(
-                                                            "Failed to parse ok type string: {}",
-                                                            e
-                                                        ),
-                                                    )
-                                                })?;
-                                            ok_type = quote! { #type_ident };
-                                        }
-                                    }
-                                }
-                                Some("err") => {
-                                    if let syn::Expr::Path(expr_path) = value {
-                                        err_type = quote! { #expr_path };
-                                    } else if let syn::Expr::Lit(lit) = value {
-                                        if let Lit::Str(lit_str) = lit.lit {
-                                            let type_ident: syn::Type =
-                                                syn::parse_str(&lit_str.value()).map_err(|e| {
-                                                    syn::Error::new_spanned(
-                                                        lit_str,
-                                                        format!(
-                                                            "Failed to parse err type string: {}",
-                                                            e
-                                                        ),
-                                                    )
-                                                })?;
-                                            err_type = quote! { #type_ident };
-                                        }
-                                    }
-                                }
+                                Some("key") => key_type = parse_type_value(value)?,
+                                Some("ok") => ok_type = parse_type_value(value)?,
+                                Some("err") => err_type = parse_type_value(value)?,
                                 _ => {}
                             }
                         }
@@ -296,4 +254,29 @@ fn extract_attribute_values(
         ok_type,
         err_type,
     })
+}
+
+/// Parse a type value from either a path expression or a string literal
+fn parse_type_value(value: syn::Expr) -> Result<proc_macro2::TokenStream, syn::Error> {
+    match value {
+        syn::Expr::Path(expr_path) => Ok(quote! { #expr_path }),
+        syn::Expr::Tuple(tuple_expr) => {
+            // Handle unit type () and tuple types
+            Ok(quote! { #tuple_expr })
+        }
+        syn::Expr::Lit(lit) => {
+            if let Lit::Str(lit_str) = lit.lit {
+                let type_ident: syn::Type = syn::parse_str(&lit_str.value()).map_err(|e| {
+                    syn::Error::new_spanned(lit_str, format!("Failed to parse type string: {}", e))
+                })?;
+                Ok(quote! { #type_ident })
+            } else {
+                Err(syn::Error::new_spanned(
+                    lit,
+                    "Expected string literal for type",
+                ))
+            }
+        }
+        _ => Err(syn::Error::new_spanned(value, "Expected path, tuple, or string literal")),
+    }
 }
